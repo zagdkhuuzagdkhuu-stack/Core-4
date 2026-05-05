40ac593f48b594439bfbe59a4d500c9ddc32b777
zagdkhuuzagdkhuu-stack <zagdkhuuzagdkhuu@gmail.com>
2026-05-05 18:04:55 +0800

    tsoo shine structure

---
+++ prisma.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "prisma/config";
+import "dotenv/config";
+export default defineConfig({
+    schema: "app/prisma/schema.prisma",
+    migrations: {
+        path: "app/prisma/migrations",
+    },
+    datasource: {
+        url: process.env["DATABASE_URL"],
+    },
+});
